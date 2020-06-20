a651ad451340aabaa330b7954fb40e0057584e85
andressalh <andressalhenriques@hotmail.com>
2020-06-20 18:17:14 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json()); //
 app.use(bodyParser.urlencoded({extended: false})); // entender quando passar paramentros url e conseguir deporar
+app.use(cors());
 
 require('./controllers/empresaController')(app);
 require('./controllers/funcionarioController')(app);
--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
+
 class Home extends Component {
 
+    componentDidMount(){
+
+        try {
+            console.log("entrou")
+            api.get('/empresa').then((res) =>{
+                console.log("entrosu")
+                console.log(res);
+            });
+            
+          } catch (error) {
+            console.error(error);
+          }
+
+    }
     render(){
 
-        api.get('/empresa').then(response => {
-            console.log(response.data);
-        });
-        
         return (
             <div id="page-home">
                 <h1>Teste</h1>
