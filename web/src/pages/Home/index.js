e8fe6a2004bb9ea80be8d784e30f9e660532e5df
andressalh <andressalhenriques@hotmail.com>
2020-06-20 17:46:08 -0300

    update

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-
+import api from '../../services/api';
 
 class Home extends Component {
 
-    state = {
-        url: 'http://localhost:3000',
-        pokemon: null
-      };
-
-      async componentDidMount(){
-          const res = await axios.get(this.state.url);
-          
-      }
-
-
     render(){
+
+        api.get('/empresa').then(response => {
+            console.log(response.data);
+        });
+        
         return (
             <div id="page-home">
                 <h1>Teste</h1>
